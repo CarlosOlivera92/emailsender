5c537e29ff8ff2590e81dba281c85601c1ed59b2
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-02-27 13:25:42 -0300

    added dynamic port using environment variables

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 dotenv.config();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3307;
 const app = express();
 
 // Permitir solicitudes desde cualquier origen
