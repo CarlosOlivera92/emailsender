c6c71c21906915621a92f09d98fc2526359ddec0
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-02-26 22:18:56 -0300

    fixed an issue that causes an error related to the login in nodemailer when environment variables were used

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,12 +2,12 @@ import nodemailer from 'nodemailer';
 import configs from '../configs/configs.js';
 const user = configs.googleUsername;
 const pass = configs.googleApiKey;
-console.log(user)
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: `olivera.carlos1999@gmail.com`,
-        pass: `vigx cwho aiov vxms`,
+        user: user,
+        pass: pass,
     }
 });
 
